ad32977066fa61bdfb304b976645ac734e02e75a
Farid Ahmad Haidary <faridahmadhaidary01@gmail.com>
2025-06-22 16:46:34 +0430

    a column by the name of video is added in Database

---
+++ database/migrations/2025_06_22_113733_add_video_to_students_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->string('video');
+            // Adding a new column 'video' to the 'students' table
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('students', function (Blueprint $table) {
+            //
+        });
+    }
+};
